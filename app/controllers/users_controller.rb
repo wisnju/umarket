5cc84431542404b7a004df3992382b211f909a9f
wiscas <tzyoungnju@foxmail.com>
2018-01-30 16:13:34 +0000

    add signup and use SSL and puma gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "注册成功！欢迎加入UMARKET！"
+      redirect_to @user
+      # 处理注册成功的情况
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 SampleApp::Application.routes.draw do
   get 'users/new'
-
   root :to      => 'static_pages#home'
- 
   get 'home'    => 'static_pages#home' 
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
 
+  resources :users
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
